05f59ea2dc267d8a513caf7ffdb78c2673c51582
Anurag Chemban <chembananurag23@gmail.com>
2024-09-27 14:48:52 +0530

    SCEngine: basic function declr

---
+++ src/SCEngine.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+/// @title SCEngine
+/// @author AGK
+/// @notice This contract is the core of SC system. It handles all the logic for minting and burning SC, as well as depositing and withdrawing collateral.
+contract SCEngine {
+    function depositCollateralAndMintSC() external {}
+
+    function depositCollateral() external {}
+
+    function redeemCollateralforSC() external {}
+
+    function redeemCollateral() external {}
+
+    function mintCollateral() external {}
+
+    function burnSC() external {}
+
+    function liquidate() external {}
+
+    function healthFactor() external view {}
+}
